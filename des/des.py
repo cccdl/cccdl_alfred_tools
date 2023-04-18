23239848b365a039e80c8e13c729ce0382ec63cd
dl_c <568078220@qq.com>
2023-04-18 17:35:47 +0800

    加密解密

--- des/des.py
+++ des/des.py
@@ -1,16 +1,30 @@
 import base64
-
+import sys
+import json
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 
-qyery = base64.b64decode(b'xF09ZQKbxCDkiiSZUWclvA==')
 
-print(qyery)
+def createItem(title, subtitle, icon, arg):
+    item = {'title': title, 'subtitle': subtitle, 'icon': icon, 'arg': arg}
+    return item
+
+
+a = sys.argv[1]
+str = a.encode()
+qyery = base64.b64decode(str)
 
 key = b'nAdYRsCATiFMXfEr'
 cipher = AES.new(key, AES.MODE_ECB)
 a = cipher.decrypt(qyery)
 
 str = unpad(a, 16)
-print(str.decode())
+b = str.decode()
 
+items = []
+
+items.append(createItem(b, '结果', {'type': 'default', 'path': 'icon.png'}, b))
+
+result = {'items': items}
+
+print(json.dumps(result))
