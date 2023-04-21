08ff0ee167ff65f628ef856fd110a1d813144d19
dl_c <568078220@qq.com>
2023-04-21 17:26:39 +0800

    优化生成密码

--- password/input.py
+++ password/input.py
@@ -1,20 +1,28 @@
 import json
+import sys
 
 item = []
-
-item.append({
+a1 = {
     'title': '简易密码',
     'subtitle': '',
     'arg': 1
-})
-
-item.append({
+}
+a2 = {
     'title': '复杂密码',
     'subtitle': '带特殊字符',
     'arg': 2
-})
+}
+item.append(a1)
+
+item.append(a2)
 
 result = {'items': item}
 
-print(json.dumps(result))
+if len(sys.argv) == 2:
+    query = sys.argv[1]
+    if query == '1':
+        item.remove(a2)
+    elif query == '2':
+        item.remove(a1)
 
+print(json.dumps(result))
